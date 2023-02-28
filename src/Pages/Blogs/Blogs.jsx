b8313f1eb21e2202bcfa23730c92aaa7839c965b
Edcreation <93517096+Edcreation@users.noreply.github.com>
2023-02-28 21:33:40 +0200

    Update Blogs.jsx

--- src/Pages/Blogs/Blogs.jsx
+++ src/Pages/Blogs/Blogs.jsx
@@ -5,7 +5,9 @@ import Dark from '../../components/Dark';
 import axios from 'axios';
 function Blogs() {
     const [blogs, setBlogs] = useState([])
+    const [item, setItem] = useState("")
     const [loading, setLoading] = useState(false)
+
     useEffect(() => {
         fetchProducts();
       }, []);
@@ -20,32 +22,50 @@ function Blogs() {
           .catch((err) => {
             console.log(err);
           });
+    };
+    
+    const searchBlogs = async (e) => {
+        e.preventDefault()
+        setLoading(true);
+        const detail = {
+            search: item
+        }
+        const rawResponse = await fetch('https://my-brand-production.up.railway.app/blogs/search', {
+            method: 'POST',
+            headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(detail)
+          });
+          const content = await rawResponse.json();
+          setBlogs(content.BlogFetched)
+          setLoading(false)
       };
       
   return (
     <div className={`blogs ${Dark()}`}>
         <div className="top-bar" >
-            <form action="">
-                <input type="text" name="search" id="search" />
-                <button><i className="fa fa-search" aria-hidden="true"></i></button>
+            <form action="" onSubmit={searchBlogs}>
+                <input type="text" value={item} onChange={e => setItem(e.target.value)} />
+                <button ><i className="fa fa-search" aria-hidden="true"></i></button>
             </form>
         </div>
         {loading ? <div className='loader'><div className="spinner"></div></div> : null}
         <div className="blogs-container">
-            { blogs.map(blog => (
-                <div className="blog-card" key={blog._id} >
-                    <div className="blog-image">
-                        <img src={blog.imageUrl} alt="image" />
-                    </div>
-                    <p>{blog.title}</p>
-                    <div className="btn">
-                        <Link to={`/single-blog/${blog._id}`} className="readmore-btn">
-                            Read More...
-                        </Link>
-                    </div>
-                </div>
-
-            ))}
+            {blogs.map(blog => (
+                    <div className="blog-card" key={blog._id} >
+                        <div className="blog-image">
+                            <img src={blog.imageUrl} alt="image" />
+                        </div>
+                        <p>{blog.title}</p>
+                        <div className="btn">
+                            <Link to={`/single-blog/${blog._id}`} className="readmore-btn">
+                                Read More...
+                            </Link>
+                        </div>
+                    </div>))
+            }
         </div>
     </div>
   )
